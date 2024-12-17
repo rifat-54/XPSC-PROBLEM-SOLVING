13410a936e120f00c12adb6b6e07bf6c3058b53b
rifat-54 <rifat999por@gmail.com>
2024-12-17 23:34:48 +0600

    added day 3

---
+++ week 8/day-3/Xorry_2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int maxbit = __lg(n);
+        // cout<<maxbit<<'\n';
+        int ctn = 1;
+        bool flag=false;
+        for (int i = maxbit-1; i >=0 ; i--)
+        {
+            if (n & (1 << i))
+            {
+                flag=true;
+            }
+            else
+            {
+                if(flag){
+
+                ctn=ctn*2;
+                }
+                
+            }
+        }
+        cout << ctn << '\n';
+    }
+
+    return 0;
+}
