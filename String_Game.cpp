a7f29f219688e4066a7a1961e3dd70323c2d315a
rifat-54 <rifat999por@gmail.com>
2024-12-19 23:23:15 +0600

    added ady 5

---
+++ String_Game.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (s[i] != s[i + 1])
+                {
+                    
+                }
+            }
+        }
+    }
+
+    return 0;
+}
